4831df463256ece968982732c42853b737645d11
Dylan Park <dylanpark@college.harvard.edu>
2024-11-15 17:33:50 -0500

    preprocessed delay data

--- js/main.js
+++ js/main.js
@@ -1,7 +1,23 @@
 
 let delayData
 d3.csv("data/Airline_Delay_cause.csv", row => {
-    // row.__  = +row.___
+    row.year  = +row.year;
+    row.month = +row.month;
+    row.arr_flights = +row.arr_flights;
+    row.arr_del15 = +row.arr_del15;
+    row.carrier_ct = +row.carrier_ct;
+    row.weather_ct = +row.weather_ct;
+    row.nas_ct = +row.nas_ct;
+    row.security_ct = +row.security_ct;
+    row.late_aircraft_ct = +row.late_aircraft_ct;
+    row.arr_cancelled = +row.arr_cancelled;
+    row.arr_diverted = +row.arr_diverted;
+    row.arr_delay = +row.arr_delay;
+    row.carrier_delay = +row.carrier_delay;
+    row.weather_delay = +row.weather_delay;
+    row.nas_delay = +row.nas_delay;
+    row.security_delay = +row.security_delay;
+    row.late_aircraft_delay = +row.late_aircraft_delay;
     return row
 }).then(csv => {
     delayData = csv
